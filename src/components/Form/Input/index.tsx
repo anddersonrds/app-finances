e3f05f0eaded3fb17eae1127e4b4c7d28fc48ced
Anderson Rodrigues <anddersonrds@gmail.com>
2021-09-08 22:32:04 -0300

    Add Input Component

---
+++ src/components/Form/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TextInputProps } from 'react-native'
+
+import * as S from './styles'
+
+export const Input = ({ ...rest }: TextInputProps) => {
+  return (
+    <S.Wrapper {...rest} />
+  )
+}
+
+export default Input
